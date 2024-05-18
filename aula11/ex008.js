82bb621319634e2dfceb95230fa9bd55c9bff17f
Rafael Nunes de Castro <160686742+rafaelnunesdecastro@users.noreply.github.com>
2024-05-18 15:46:21 -0300

    exercícios 008,009,010e011

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.2
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel > 60){
+    console.log(`Você ultrapasou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
